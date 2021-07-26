3ce3d13303bf8e5c44516a4e3e1cae0ed79b6816
Maksym Zabolotnyi <maxim.zabolotny20121776@gmail.com>
2021-07-26 09:03:32 +0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { CategoriesTree } from "./interfaces/CategoriesTree";
+import { Parser, PathBuilder } from "./parser";
+
+const csvFileUrl = "https://pics.ebaystatic.com/aw/pics/catchanges/US_NewStructure(Oct2019).csv"
+
+const parser = new Parser(csvFileUrl)
+const pathBuilder = new PathBuilder()
+const startScript = async () => {
+    await parser.downloadFile()
+    await parser.toJson()
+    await parser.saveJson()
+    const path = pathBuilder.findPath('73464')
+    const res: CategoriesTree = parser.getResult()
+    console.log(res)
+    console.log(path);
+
+}
+
+startScript()
